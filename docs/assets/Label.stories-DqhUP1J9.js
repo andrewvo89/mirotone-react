ec939978671b31f684619f7a7c0e59f78d72fbdc
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-03 19:12:57 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@511a5c151b07f88060559efa1da3de5c1d15563b 🚀

---
+++ docs/assets/Label.stories-DqhUP1J9.js
@@ -0,0 +1,9 @@
+import{L as u}from"./TagButton-DLLDeqst.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const v={title:"Components/Label",component:u,args:{variant:"info"},parameters:{layout:"centered"},tags:["autodocs"]},r={},a={args:{variant:"info"}},e={args:{variant:"warning"}};var o,s,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(n=(s=r.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var t,c,i;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    variant: 'info'
+  }
+}`,...(i=(c=a.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var m,p,d;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: 'warning'
+  }
+}`,...(d=(p=e.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const L=["Default","Info","Warning"];export{r as Default,a as Info,e as Warning,L as __namedExportsOrder,v as default};
---
+++ docs/assets/Link.stories-8GCQAAgz.js
@@ -0,0 +1,13 @@
+import{k as x}from"./TagButton-DLLDeqst.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const S={title:"Components/Link",component:x,args:{variant:"primary"},parameters:{layout:"centered"},tags:["autodocs"]},r={},a={args:{variant:"primary"}},e={args:{variant:"danger"}},s={args:{variant:"text"}};var t,o,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var c,m,p;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary'
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var i,d,u;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    variant: 'danger'
+  }
+}`,...(u=(d=e.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,l,v;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    variant: 'text'
+  }
+}`,...(v=(l=s.parameters)==null?void 0:l.docs)==null?void 0:v.source}}};const k=["Default","Primary","Danger","Text"];export{e as Danger,r as Default,a as Primary,s as Text,k as __namedExportsOrder,S as default};
