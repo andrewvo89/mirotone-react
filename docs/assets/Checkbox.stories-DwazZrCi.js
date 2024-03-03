34a758b55d89ceb57a00accd9a136ff392a33f63
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-03 19:53:22 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@700dd9c48b4456033744d53f5166f5ed93870ecc 🚀

---
+++ docs/assets/Checkbox.stories-DwazZrCi.js
@@ -0,0 +1,19 @@
+import{C as i,j as x}from"./TagButton-CSo3gd_q.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const{useArgs:_}=__STORYBOOK_MODULE_PREVIEW_API__,E={title:"Components/Checkbox",component:i,parameters:{layout:"centered"},tags:["autodocs"]},o={},n={args:{value:!1},render:function(e){const[,h]=_(),b=a=>{var s;(s=e.onChange)==null||s.call(e,a),h({value:a})};return x.jsx(i,{...e,value:e.value,onChange:b})}},r={args:{label:"Checkbox"}};var t,c,l;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(l=(c=o.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var u,m,p;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    value: false
+  },
+  render: function Component(args) {
+    const [, setArgs] = useArgs();
+    const onChange = (value: boolean) => {
+      args.onChange?.(value);
+      setArgs({
+        value
+      });
+    };
+    return <Checkbox {...args} value={args.value} onChange={onChange} />;
+  }
+}`,...(p=(m=n.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var d,g,C;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox'
+  }
+}`,...(C=(g=r.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};const O=["Default","Controlled","Label"];export{n as Controlled,o as Default,r as Label,O as __namedExportsOrder,E as default};
---
+++ docs/assets/RadioButton.stories-C6plHdhC.js
@@ -0,0 +1,25 @@
+import{R as n,j as a}from"./TagButton-CSo3gd_q.js";import{r as h}from"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const{useArgs:j}=__STORYBOOK_MODULE_PREVIEW_API__,D={title:"Components/RadioButton",component:n,parameters:{layout:"centered"},tags:["autodocs"]},o={},r={args:{value:!1},render:function(e){const[,_]=j(),v=l=>{var c;(c=e.onChange)==null||c.call(e,l),_({value:l})};return a.jsx(n,{...e,value:e.value,onChange:v})}},t={args:{label:"Option 1"}},s={render:()=>a.jsxs(h.Fragment,{children:[a.jsx(n,{label:"Option 1",name:"radio"}),a.jsx(n,{label:"Option 2",name:"radio"}),a.jsx(n,{label:"Option 3",name:"radio"})]})};var u,i,m;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(m=(i=o.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,d,g;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    value: false
+  },
+  render: function Component(args) {
+    const [, setArgs] = useArgs();
+    const onChange = (value: boolean) => {
+      args.onChange?.(value);
+      setArgs({
+        value
+      });
+    };
+    return <RadioButton {...args} value={args.value} onChange={onChange} />;
+  }
+}`,...(g=(d=r.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var O,b,C;t.parameters={...t.parameters,docs:{...(O=t.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    label: 'Option 1'
+  }
+}`,...(C=(b=t.parameters)==null?void 0:b.docs)==null?void 0:C.source}}};var x,R,f;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => <Fragment>
+      <RadioButton label='Option 1' name='radio' />
+      <RadioButton label='Option 2' name='radio' />
+      <RadioButton label='Option 3' name='radio' />
+    </Fragment>
+}`,...(f=(R=s.parameters)==null?void 0:R.docs)==null?void 0:f.source}}};const F=["Default","Controlled","Label","Multiple"];export{r as Controlled,o as Default,t as Label,s as Multiple,F as __namedExportsOrder,D as default};
