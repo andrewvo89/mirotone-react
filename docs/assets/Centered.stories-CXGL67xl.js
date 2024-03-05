113a39a2294d43c010e50a30ec18b994229671bb
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-05 07:06:54 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@983f619677d496c18b431f8783379e6cbc7c1985 🚀

---
+++ docs/assets/Centered.stories-CXGL67xl.js
@@ -0,0 +1,9 @@
+import{y as n,j as e}from"./GridItem-DyOCgcsL.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const o=d=>e.jsx("div",{style:{height:"500px",padding:"var(--space-small)",width:"338px",display:"block",border:"2px solid var(--indigo300)",borderRadius:"var(--border-radius-medium)",margin:"var(--space-small) auto",background:"var(--white)"},children:d.children}),i=()=>e.jsx("div",{style:{width:"240px",height:"110px",backgroundColor:"var(--indigo200)",display:"flex",borderRadius:"var(--border-radius-medium)",justifyContent:"center",alignItems:"center"}}),p={title:"Layout/Centered",component:n,parameters:{layout:"centered"},tags:["autodocs"]},r={render:()=>e.jsx(o,{children:e.jsxs(n,{children:[e.jsx("h1",{className:"h1",children:"Header 1"}),e.jsx(i,{}),e.jsx("p",{className:"p-medium",children:"test"})]})})};var a,s,t;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => <Container>
+      <Centered>
+        <h1 className='h1'>Header 1</h1>
+        <Skeleton />
+        <p className='p-medium'>test</p>
+      </Centered>
+    </Container>
+}`,...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,p as default};
