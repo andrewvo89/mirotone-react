ea6a8bf99188f82170287771271977a9c4080062
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-05 07:17:05 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@c45f80305b30ae79e887c19a89209f974d260ee7 🚀

---
+++ docs/assets/Button.stories-Dr_etCGB.js
@@ -0,0 +1,37 @@
+import{B as H}from"./GridItem-BGR8QeLg.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const N={title:"Components/Button",component:H,args:{children:"Button",isDisabled:!1,isLoading:!1,size:"medium",variant:"primary"},parameters:{layout:"centered"},tags:["autodocs"]},r={},a={args:{variant:"primary"}},e={args:{variant:"secondary"}},s={args:{variant:"tertiary"}},o={args:{variant:"danger"}},t={args:{size:"small"}},n={args:{size:"medium"}},c={args:{size:"large"}},i={args:{isDisabled:!0}},m={args:{isLoading:!0}};var d,p,u;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(u=(p=r.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var g,l,y;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary'
+  }
+}`,...(y=(l=a.parameters)==null?void 0:l.docs)==null?void 0:y.source}}};var S,v,D;e.parameters={...e.parameters,docs:{...(S=e.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(D=(v=e.parameters)==null?void 0:v.docs)==null?void 0:D.source}}};var z,L,f;s.parameters={...s.parameters,docs:{...(z=s.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    variant: 'tertiary'
+  }
+}`,...(f=(L=s.parameters)==null?void 0:L.docs)==null?void 0:f.source}}};var b,B,x;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    variant: 'danger'
+  }
+}`,...(x=(B=o.parameters)==null?void 0:B.docs)==null?void 0:x.source}}};var M,P,T;t.parameters={...t.parameters,docs:{...(M=t.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(T=(P=t.parameters)==null?void 0:P.docs)==null?void 0:T.source}}};var _,h,C;n.parameters={...n.parameters,docs:{...(_=n.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...(C=(h=n.parameters)==null?void 0:h.docs)==null?void 0:C.source}}};var E,O,j;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(j=(O=c.parameters)==null?void 0:O.docs)==null?void 0:j.source}}};var k,q,w;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(w=(q=i.parameters)==null?void 0:q.docs)==null?void 0:w.source}}};var A,F,G;m.parameters={...m.parameters,docs:{...(A=m.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    isLoading: true
+  }
+}`,...(G=(F=m.parameters)==null?void 0:F.docs)==null?void 0:G.source}}};const Q=["Default","Primary","Secondary","Tertiary","Danger","Small","Medium","Large","Disabled","Loading"];export{o as Danger,r as Default,i as Disabled,c as Large,m as Loading,n as Medium,a as Primary,e as Secondary,t as Small,s as Tertiary,Q as __namedExportsOrder,N as default};
