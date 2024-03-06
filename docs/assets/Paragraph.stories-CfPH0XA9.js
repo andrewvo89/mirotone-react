b36659c0c744b717d8a9029923adfe84a7f7a4c6
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-06 19:57:24 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@8ce2db0718b0c9548c548b1af5f80d820c53b866 🚀

---
+++ docs/assets/Paragraph.stories-CfPH0XA9.js
@@ -0,0 +1,16 @@
+import{P as d}from"./Text-CBxG_OWd.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const z={title:"Typography/Paragraph",component:d,parameters:{layout:"centered"},tags:["autodocs"]},r={args:{children:"Paragraph large",size:"large"}},a={args:{children:"Paragraph medium",size:"medium"}},e={args:{children:"Paragraph small",size:"small"}};var s,o,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'Paragraph large',
+    size: 'large'
+  }
+}`,...(m=(o=r.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};var n,t,c;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: 'Paragraph medium',
+    size: 'medium'
+  }
+}`,...(c=(t=a.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};var p,i,l;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: 'Paragraph small',
+    size: 'small'
+  }
+}`,...(l=(i=e.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const S=["Large","Medium","Small"];export{r as Large,a as Medium,e as Small,S as __namedExportsOrder,z as default};
---
+++ docs/assets/getting-started-e5VaulR9.js
@@ -0,0 +1,89 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as s}from"./index-Bf3ws-ph.js";import{useMDXComponents as o}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Cu78oVf9.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";function t(r){const n=Object.assign({h1:"h1",p:"p",a:"a",h2:"h2",ul:"ul",li:"li",pre:"pre",code:"code"},o(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Getting started"}),`
+`,e.jsx(n.h1,{id:"getting-started",children:"Getting started"}),`
+`,e.jsxs(n.p,{children:["Mirotone React is a component library for the Mirotone Design System. It provides a set of components and utilities to help you build faster for Miro. It is loosely based off ",e.jsx(n.a,{href:"https://www.mirotone.xyz/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Mirotone CSS"})," and ",e.jsx(n.a,{href:"https://www.figma.com/community/file/1125698230201231315/mirotone-ui-components",target:"_blank",rel:"nofollow noopener noreferrer",children:"Miro UI Components"}),"."]}),`
+`,e.jsx(n.h2,{id:"table-of-contents",children:"Table of contents"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#installation",children:"Installation"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#usage",children:"Usage"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#components",children:"Tokens"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#roadmap",children:"Roadmap"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#versions",children:"Versions"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#license",children:"License"})}),`
+`]}),`
+`,e.jsx(n.h2,{id:"installation",children:"Installation"}),`
+`,e.jsx(n.p,{children:"Install the package as a dependency on your own project by running:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`npm install mirotone-react
+`})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`yarn add mirotone-react
+`})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`pnpm add mirotone-react
+`})}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(n.p,{children:"All components and types are exported from the main package. You can import them as follows:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { Button, ButtonProps, Input, InputProps, tokens } from 'mirotone-react';
+import { useCallback, useRef, useState } from 'react';
+
+const buttonProps: ButtonProps = {
+  size: 'large',
+  variant: 'danger',
+};
+
+const inputProps: InputProps = {
+  size: 'small',
+};
+
+function App() {
+  const [value, setValue] = useState('');
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const buttonClickHandler = useCallback(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <>
+      <Input
+        {...inputProps}
+        ref={inputRef}
+        value={value}
+        onChange={setValue}
+        style={{ color: tokens.color.red[800] }}
+      />
+      <Button
+        {...buttonProps}
+        ref={buttonRef}
+        style={{ borderRadius: tokens.borderRadius.small }}
+        onClick={buttonClickHandler}
+      >
+        Click to focus input
+      </Button>
+    </>
+  );
+}
+
+export default App;
+`})}),`
+`,e.jsx(n.h2,{id:"tokens",children:"Tokens"}),`
+`,e.jsx(n.p,{children:`Mirotone React provides a type safe way to access the tokens from Mirotone CSS.
+Tokens include color, typography, space, borderRadius.
+All tokens are exported from the main package. You can import them as follows:`}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { tokens } from 'mirotone-react';
+`})}),`
+`,e.jsxs("table",{children:[e.jsx("thead",{children:e.jsxs("tr",{children:[e.jsx("th",{children:"Token"}),e.jsx("th",{children:"Path"}),e.jsx("th",{children:"Documentation"})]})}),e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Color"}),e.jsx("td",{children:e.jsx(n.code,{children:"tokens.color"})}),e.jsx("td",{children:e.jsx(n.a,{href:"?path=/docs/tokens-colors--docs",children:"Color docs"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"Typography"}),e.jsx("td",{children:e.jsx(n.code,{children:"tokens.typography"})}),e.jsx("td",{children:e.jsx(n.a,{href:"?path=/docs/tokens-typography--docs",children:"Typography docs"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"Space"}),e.jsx("td",{children:e.jsx(n.code,{children:"tokens.space"})}),e.jsx("td",{children:e.jsx(n.a,{href:"?path=/docs/tokens-space--docs",children:"Space docs"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"Border radius"}),e.jsx("td",{children:e.jsx(n.code,{children:"tokens.borderRadius"})}),e.jsx("td",{children:e.jsx(n.a,{href:"?path=/docs/tokens-border-radius--docs",children:"Border radius docs"})})]})]})]}),`
+`,e.jsx(n.h2,{id:"roadmap",children:"Roadmap"}),`
+`,e.jsxs("table",{children:[e.jsx("thead",{children:e.jsxs("tr",{children:[e.jsx("th",{children:"Feature"}),e.jsx("th",{children:"Status"}),e.jsx("th",{children:"Reference"})]})}),e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"App footer"}),e.jsx("td",{children:"Planned"}),e.jsx("td",{children:e.jsx(n.p,{children:e.jsx(n.a,{href:"https://www.figma.com/file/BI4RtvvajB7E3EQHlNTL4I/Mirotone-UI-Components-(Community)?type=design&node-id=3642-15152&mode=design&t=lya7mgGuGGuYILSD-4",target:"_blank",rel:"nofollow noopener noreferrer",children:`Figma
+design`})})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"App header"}),e.jsx("td",{children:"Planned"}),e.jsx("td",{children:e.jsx(n.p,{children:e.jsx(n.a,{href:"https://www.figma.com/file/BI4RtvvajB7E3EQHlNTL4I/Mirotone-UI-Components-(Community)?type=design&node-id=3642-15175&mode=design&t=M6wFp57pRdq4g6Te-4",target:"_blank",rel:"nofollow noopener noreferrer",children:`Figma
+design`})})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"App nav"}),e.jsx("td",{children:"Planned"}),e.jsx("td",{children:e.jsx(n.p,{children:e.jsx(n.a,{href:"https://www.figma.com/file/BI4RtvvajB7E3EQHlNTL4I/Mirotone-UI-Components-(Community)?type=design&node-id=3642-15189&mode=design&t=M6wFp57pRdq4g6Te-4",target:"_blank",rel:"nofollow noopener noreferrer",children:`Figma
+design`})})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"Dismissable badge"}),e.jsx("td",{children:"Planned"}),e.jsx("td",{children:e.jsx(n.p,{children:e.jsx(n.a,{href:"https://www.figma.com/file/BI4RtvvajB7E3EQHlNTL4I/Mirotone-UI-Components-(Community)?type=design&node-id=3642-13487&mode=design&t=lya7mgGuGGuYILSD-4",target:"_blank",rel:"nofollow noopener noreferrer",children:`Figma
+design`})})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"Dropdown button"}),e.jsx("td",{children:"Planned"}),e.jsx("td",{children:e.jsx(n.p,{children:e.jsx(n.a,{href:"https://www.figma.com/file/BI4RtvvajB7E3EQHlNTL4I/Mirotone-UI-Components-(Community)?type=design&node-id=3642-13529&mode=design&t=lya7mgGuGGuYILSD-4",target:"_blank",rel:"nofollow noopener noreferrer",children:`Figma
+design`})})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"Icon button"}),e.jsx("td",{children:"Planned"}),e.jsx("td",{children:e.jsx(n.p,{children:e.jsx(n.a,{href:"https://www.figma.com/file/BI4RtvvajB7E3EQHlNTL4I/Mirotone-UI-Components-(Community)?type=design&node-id=3642-13902&mode=design&t=lya7mgGuGGuYILSD-4",target:"_blank",rel:"nofollow noopener noreferrer",children:`Figma
+design`})})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"Icon text button"}),e.jsx("td",{children:"Planned"}),e.jsx("td",{children:e.jsx(n.p,{children:e.jsx(n.a,{href:"https://www.figma.com/file/BI4RtvvajB7E3EQHlNTL4I/Mirotone-UI-Components-(Community)?type=design&node-id=3642-13693&mode=design&t=lya7mgGuGGuYILSD-4",target:"_blank",rel:"nofollow noopener noreferrer",children:`Figma
+design`})})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"Tooltip"}),e.jsx("td",{children:"Planned"}),e.jsx("td",{children:e.jsx(n.p,{children:e.jsx(n.a,{href:"https://www.figma.com/file/BI4RtvvajB7E3EQHlNTL4I/Mirotone-UI-Components-(Community)?type=design&node-id=3642-13308&mode=design&t=v8b8OhxAH4M1Erx0-4",target:"_blank",rel:"nofollow noopener noreferrer",children:`Figma
+design`})})})]})]})]}),`
+`,e.jsx(n.h2,{id:"versions",children:"Versions"}),`
+`,e.jsxs("table",{children:[e.jsx("thead",{children:e.jsxs("tr",{children:[e.jsx("th",{children:"Mirotone React"}),e.jsx("th",{children:"Mirotone CSS"}),e.jsx("th",{children:"Mirotone UI Components"})]})}),e.jsx("tbody",{children:e.jsxs("tr",{children:[e.jsx("td",{children:"1.0.0"}),e.jsx("td",{children:"5.1.1"}),e.jsx("td",{children:"1.4"})]})})]}),`
+`,e.jsx(n.h2,{id:"license",children:"License"}),`
+`,e.jsxs(n.p,{children:["Mirotone React is distributed under MIT license, Copyright (c) 2024 Andrew Vo-Nguyen. See ",e.jsx(n.a,{href:"https://github.com/andrewvo89/mirotone-react/blob/main/LICENSE",target:"_blank",rel:"nofollow noopener noreferrer",children:"LICENSE"})," for more information."]})]})}function f(r={}){const{wrapper:n}=Object.assign({},o(),r.components);return n?e.jsx(n,Object.assign({},r,{children:e.jsx(t,r)})):t(r)}export{f as default};
