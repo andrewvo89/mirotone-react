5b685894e575573f469f3f4aeb90d9ae6a22e597
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-06 05:37:54 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@b211c00e8490839b0924782c33dc74e590ea9d40 🚀

---
+++ docs/assets/Heading.stories-Byus-tc_.js
@@ -0,0 +1,21 @@
+import{H as h}from"./Text-Dud5oAvr.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const x={title:"Typography/Heading",component:h,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{children:"Heading 1",level:1}},r={args:{children:"Heading 2",level:2}},a={args:{children:"Heading 3",level:3}},n={args:{children:"Heading 4",level:4}};var s,o,d;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 1',
+    level: 1
+  }
+}`,...(d=(o=e.parameters)==null?void 0:o.docs)==null?void 0:d.source}}};var c,i,t;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 2',
+    level: 2
+  }
+}`,...(t=(i=r.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};var g,l,p;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 3',
+    level: 3
+  }
+}`,...(p=(l=a.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var m,H,u;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 4',
+    level: 4
+  }
+}`,...(u=(H=n.parameters)==null?void 0:H.docs)==null?void 0:u.source}}};const _=["Heading1","Heading2","Heading3","Heading4"];export{e as Heading1,r as Heading2,a as Heading3,n as Heading4,_ as __namedExportsOrder,x as default};
