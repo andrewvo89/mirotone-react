6aec0b0ef7ae039fcbfc51a7e9d8549b4bd61390
Andrew Vo-Nguyen <andrew.vo89@gmail.com>
2024-03-07 15:10:25 +1100

    Update Storybook workflow to deploy to GitHub Pages; adjust staticDirs config.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,6 +16,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
-  staticDirs: [{ from: '../public', to: '../public' }],
+  staticDirs: ['../public'],
 };
 export default config;
