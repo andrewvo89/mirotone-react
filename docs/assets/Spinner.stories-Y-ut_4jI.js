ab0cc0963bf73e429d3b2ceda9a010050b75d59d
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-07 05:58:44 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@cbfa2bc38d8f2b4e5fabf25f12538789f403a8b0 🚀

---
+++ docs/assets/Spinner.stories-Y-ut_4jI.js
@@ -0,0 +1,13 @@
+import{n as f}from"./Text-BnStar7d.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const x={title:"Components/Spinner",component:f,argTypes:{size:{defaultValue:{summary:"medium"}},spinnerColor:{defaultValue:{summary:"var(--indigo700)"}}},parameters:{layout:"centered"},tags:["autodocs"]},r={},e={args:{size:"small"}},a={args:{size:"large"}},s={args:{spinnerColor:"var(--red900)"}};var o,t,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var m,c,p;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(p=(c=e.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var l,i,d;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(d=(i=a.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var u,g,S;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    spinnerColor: 'var(--red900)'
+  }
+}`,...(S=(g=s.parameters)==null?void 0:g.docs)==null?void 0:S.source}}};const D=["Default","Small","Large","Colored"];export{s as Colored,r as Default,a as Large,e as Small,D as __namedExportsOrder,x as default};
