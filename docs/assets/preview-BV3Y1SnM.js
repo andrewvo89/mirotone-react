c5bc56fa5a1ba651c4eced8ca6d7e225f084f2da
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-10 18:55:05 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@b6814f416ac4c41fd1a7b7c48ad15317427a0ab3 🚀

--- docs/assets/preview-BV3Y1SnM.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-CIHBmm6f.js","./iframe-BylJUMaI.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-DVNvY8XX.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as e}from"./iframe-BylJUMaI.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-CIHBmm6f.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
