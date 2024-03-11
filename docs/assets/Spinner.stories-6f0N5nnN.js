95f5a04e5495da7f8bf5344ed7a30e0d3b00373b
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-11 11:04:57 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@25638cd2501026484dd2a909883cbf4814692dc3 🚀

---
+++ docs/assets/Spinner.stories-6f0N5nnN.js
@@ -0,0 +1,17 @@
+import{q as v}from"./Text-vapKb_Af.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";const q={title:"Components/Spinner",component:v,argTypes:{size:{defaultValue:{summary:"medium"}},spinnerColor:{defaultValue:{summary:"var(--indigo700)"}}},parameters:{layout:"centered"},tags:["autodocs"]},r={},e={args:{size:"small"}},a={args:{size:"medium"}},s={args:{size:"large"}},o={args:{spinnerColor:"var(--red900)"}};var t,m,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(n=(m=r.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var c,p,i;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(i=(p=e.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var d,u,l;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...(l=(u=a.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var g,S,z;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(z=(S=s.parameters)==null?void 0:S.docs)==null?void 0:z.source}}};var f,C,y;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    spinnerColor: 'var(--red900)'
+  }
+}`,...(y=(C=o.parameters)==null?void 0:C.docs)==null?void 0:y.source}}};const E=["Default","Small","Medium","Large","Colored"];export{o as Colored,r as Default,s as Large,a as Medium,e as Small,E as __namedExportsOrder,q as default};
---
+++ docs/assets/Toggle.stories-BcLJzT0s.js
@@ -0,0 +1,19 @@
+import{j as v}from"./jsx-runtime-CKrituN3.js";import{H as C}from"./Text-vapKb_Af.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";const{useArgs:h}=__STORYBOOK_MODULE_PREVIEW_API__,S={title:"Components/Toggle",component:C,parameters:{layout:"centered"},tags:["autodocs"]},o={},r={args:{value:!1},render:function(e){const[,f]=h(),_=a=>{var s;(s=e.onChange)==null||s.call(e,a),f({value:a})};return v.jsx(C,{...e,value:e.value,onChange:_})}},n={args:{label:"Toggle"}};var t,l,c;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(c=(l=o.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var u,g,m;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    value: false
+  },
+  render: function Component(args) {
+    const [, setArgs] = useArgs();
+    const onChangeHandler = (value: boolean) => {
+      args.onChange?.(value);
+      setArgs({
+        value
+      });
+    };
+    return <Toggle {...args} value={args.value} onChange={onChangeHandler} />;
+  }
+}`,...(m=(g=r.parameters)==null?void 0:g.docs)==null?void 0:m.source}}};var p,d,i;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: 'Toggle'
+  }
+}`,...(i=(d=n.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};const j=["Default","Controlled","Label"];export{r as Controlled,o as Default,n as Label,j as __namedExportsOrder,S as default};
