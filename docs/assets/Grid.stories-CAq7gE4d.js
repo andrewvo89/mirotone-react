b11ed2b5de60d31bd7e1bb79c2c7064630fa3046
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2024-03-12 10:30:14 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@1b5948b828a1771d838bf767fe8838643356eb5e 🚀

---
+++ docs/assets/Grid.stories-CAq7gE4d.js
@@ -0,0 +1,43 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{G as i,o as c}from"./Text-VnyhRAJn.js";import{t as n}from"./index-BVjCO-_d.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";function t(d){return e.jsx("div",{style:{display:"flex",height:"74px",width:"100%",backgroundColor:n.color.indigo[200],borderRadius:n.borderRadius.medium,justifyContent:"center",alignItems:"center"},children:e.jsx("code",{children:d.children})})}try{t.displayName="GridItemText",t.__docgenInfo={description:"",displayName:"GridItemText",props:{}}}catch{}const I={title:"Layout/Grid",component:i,parameters:{layout:"padded"},tags:["autodocs"]},r={parameters:{layout:"padded"},render:()=>e.jsxs(i,{children:[e.jsx(c,{colStart:1,colEnd:2,children:e.jsx(t,{children:"cs1 ce2"})}),e.jsx(c,{colStart:3,colEnd:7,children:e.jsx(t,{children:"cs3 ce7"})}),e.jsx(c,{colStart:8,colEnd:10,children:e.jsx(t,{children:"cs8 ce10"})}),e.jsx(c,{colStart:11,colEnd:12,children:e.jsx(t,{children:"cs11 ce12"})}),e.jsx(c,{colStart:1,colEnd:8,children:e.jsx(t,{children:"cs1 ce8"})}),e.jsx(c,{colStart:9,colEnd:12,children:e.jsx(t,{children:"cs9 ce12"})}),e.jsx(c,{colStart:1,colEnd:5,children:e.jsx(t,{children:"cs1 ce5"})}),e.jsx(c,{colStart:6,colEnd:12,children:e.jsx(t,{children:"cs6 ce12"})}),e.jsx(c,{colStart:1,colEnd:3,children:e.jsx(t,{children:"cs1 ce3"})}),e.jsx(c,{colStart:4,colEnd:6,children:e.jsx(t,{children:"cs4 ce6"})}),e.jsx(c,{colStart:7,colEnd:9,children:e.jsx(t,{children:"cs7 ce9"})}),e.jsx(c,{colStart:10,colEnd:12,children:e.jsx(t,{children:"cs10 ce12"})})]})};var o,l,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'padded'
+  },
+  render: () => <Grid>
+      <GridItem colStart={1} colEnd={2}>
+        <Text>cs1 ce2</Text>
+      </GridItem>
+      <GridItem colStart={3} colEnd={7}>
+        <Text>cs3 ce7</Text>
+      </GridItem>
+      <GridItem colStart={8} colEnd={10}>
+        <Text>cs8 ce10</Text>
+      </GridItem>
+      <GridItem colStart={11} colEnd={12}>
+        <Text>cs11 ce12</Text>
+      </GridItem>
+      <GridItem colStart={1} colEnd={8}>
+        <Text>cs1 ce8</Text>
+      </GridItem>
+      <GridItem colStart={9} colEnd={12}>
+        <Text>cs9 ce12</Text>
+      </GridItem>
+      <GridItem colStart={1} colEnd={5}>
+        <Text>cs1 ce5</Text>
+      </GridItem>
+      <GridItem colStart={6} colEnd={12}>
+        <Text>cs6 ce12</Text>
+      </GridItem>
+      <GridItem colStart={1} colEnd={3}>
+        <Text>cs1 ce3</Text>
+      </GridItem>
+      <GridItem colStart={4} colEnd={6}>
+        <Text>cs4 ce6</Text>
+      </GridItem>
+      <GridItem colStart={7} colEnd={9}>
+        <Text>cs7 ce9</Text>
+      </GridItem>
+      <GridItem colStart={10} colEnd={12}>
+        <Text>cs10 ce12</Text>
+      </GridItem>
+    </Grid>
+}`,...(s=(l=r.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};const T=["Default"];export{r as Default,T as __namedExportsOrder,I as default};
---
+++ docs/assets/Heading.stories-WgV_-YXW.js
@@ -0,0 +1,21 @@
+import{J as h}from"./Text-VnyhRAJn.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";const _={title:"Typography/Heading",component:h,argTypes:{level:{defaultValue:{summary:1}}},parameters:{layout:"centered"},tags:["autodocs"]},e={args:{children:"Heading 1",level:1}},r={args:{children:"Heading 2",level:2}},a={args:{children:"Heading 3",level:3}},n={args:{children:"Heading 4",level:4}};var s,o,d;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 1',
+    level: 1
+  }
+}`,...(d=(o=e.parameters)==null?void 0:o.docs)==null?void 0:d.source}}};var c,i,t;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 2',
+    level: 2
+  }
+}`,...(t=(i=r.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};var l,g,m;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 3',
+    level: 3
+  }
+}`,...(m=(g=a.parameters)==null?void 0:g.docs)==null?void 0:m.source}}};var p,u,H;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 4',
+    level: 4
+  }
+}`,...(H=(u=n.parameters)==null?void 0:u.docs)==null?void 0:H.source}}};const E=["Heading1","Heading2","Heading3","Heading4"];export{e as Heading1,r as Heading2,a as Heading3,n as Heading4,E as __namedExportsOrder,_ as default};
