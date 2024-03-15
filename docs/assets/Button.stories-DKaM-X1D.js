c96d178f3a8b689348d2e536c4b33ed3a7bbd63e
andrewvo89 <andrewvo89@users.noreply.github.com>
2024-03-15 03:34:48 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@5a0b303438263f7f4339f9c51efaea18ed4076d5 🚀

---
+++ docs/assets/Button.stories-DKaM-X1D.js
@@ -0,0 +1,54 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{B as W,I as U,T as X}from"./Text-CqINtZk8.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";const Y={title:"Components/Button",component:W,argTypes:{size:{defaultValue:{summary:"medium"}},variant:{defaultValue:{summary:"primary"}},isDisabled:{defaultValue:{summary:!1}},isLoading:{defaultValue:{summary:!1}}},args:{children:"Button"},parameters:{layout:"centered"},tags:["autodocs"]},nr=Y,e={},a={args:{variant:"primary"}},s={args:{variant:"secondary"}},t={args:{variant:"tertiary"}},n={args:{variant:"danger"}},o={args:{size:"small"}},c={args:{size:"medium"}},i={args:{size:"large"}},m={args:{isDisabled:!0}},u={args:{isLoading:!0}},d={name:"Icon button",args:{children:r.jsx(U,{name:"activity",style:{pointerEvents:"none"}})}},p={name:"Icon & text button",args:{children:r.jsxs(r.Fragment,{children:[r.jsx(U,{name:"activity",style:{pointerEvents:"none"}}),r.jsx(X,{children:"Icon & text button"})]})}};var l,g,y;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(y=(g=e.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var v,x,S;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary'
+  }
+}`,...(S=(x=a.parameters)==null?void 0:x.docs)==null?void 0:S.source}}};var I,b,f;s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(f=(b=s.parameters)==null?void 0:b.docs)==null?void 0:f.source}}};var D,T,B;t.parameters={...t.parameters,docs:{...(D=t.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    variant: 'tertiary'
+  }
+}`,...(B=(T=t.parameters)==null?void 0:T.docs)==null?void 0:B.source}}};var h,z,L;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    variant: 'danger'
+  }
+}`,...(L=(z=n.parameters)==null?void 0:z.docs)==null?void 0:L.source}}};var j,E,V;o.parameters={...o.parameters,docs:{...(j=o.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(V=(E=o.parameters)==null?void 0:E.docs)==null?void 0:V.source}}};var M,P,_;c.parameters={...c.parameters,docs:{...(M=c.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...(_=(P=c.parameters)==null?void 0:P.docs)==null?void 0:_.source}}};var C,F,O;i.parameters={...i.parameters,docs:{...(C=i.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(O=(F=i.parameters)==null?void 0:F.docs)==null?void 0:O.source}}};var R,$,k;m.parameters={...m.parameters,docs:{...(R=m.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(k=($=m.parameters)==null?void 0:$.docs)==null?void 0:k.source}}};var q,w,A;u.parameters={...u.parameters,docs:{...(q=u.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    isLoading: true
+  }
+}`,...(A=(w=u.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};var G,H,J;d.parameters={...d.parameters,docs:{...(G=d.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  name: 'Icon button',
+  args: {
+    children: <Icon name='activity' style={{
+      pointerEvents: 'none'
+    }} />
+  }
+}`,...(J=(H=d.parameters)==null?void 0:H.docs)==null?void 0:J.source}}};var K,N,Q;p.parameters={...p.parameters,docs:{...(K=p.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  name: 'Icon & text button',
+  args: {
+    children: <>
+        <Icon name='activity' style={{
+        pointerEvents: 'none'
+      }} />
+        <Text>Icon & text button</Text>
+      </>
+  }
+}`,...(Q=(N=p.parameters)==null?void 0:N.docs)==null?void 0:Q.source}}};const or=["Default","Primary","Secondary","Tertiary","Danger","Small","Medium","Large","Disabled","Loading","IconButton","IconTextButton"];export{n as Danger,e as Default,m as Disabled,d as IconButton,p as IconTextButton,i as Large,u as Loading,c as Medium,a as Primary,s as Secondary,o as Small,t as Tertiary,or as __namedExportsOrder,nr as default};
