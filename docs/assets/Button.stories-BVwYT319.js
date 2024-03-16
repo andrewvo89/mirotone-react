39cc91855b49f4d44cb13df0b31520301b8dc835
andrewvo89 <andrewvo89@users.noreply.github.com>
2024-03-16 02:02:37 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@ffd956ba9b0fb64e4449db952e401f032dd30ceb 🚀

---
+++ docs/assets/Button.stories-BVwYT319.js
@@ -0,0 +1,64 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{B as re,I as g,T as ee,t as ae}from"./Text--WihgvuV.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";const se={title:"Components/Button",component:re,argTypes:{size:{defaultValue:{summary:"medium"}},variant:{defaultValue:{summary:"primary"}},isDisabled:{defaultValue:{summary:!1}},isLoading:{defaultValue:{summary:!1}}},args:{children:"Button"},parameters:{layout:"centered"},tags:["autodocs"]},ue=se,r={},a={args:{variant:"primary"}},s={args:{variant:"secondary"}},n={args:{variant:"tertiary"}},t={args:{variant:"danger"}},o={args:{size:"small"}},c={args:{size:"medium"}},i={args:{size:"large"}},m={args:{isDisabled:!0}},u={args:{isLoading:!0}},d={name:"Icon button",args:{children:e.jsx(g,{name:"activity",style:{pointerEvents:"none"}})}},p={name:"Icon & text button",args:{children:e.jsxs(e.Fragment,{children:[e.jsx(g,{name:"activity",style:{pointerEvents:"none"}}),e.jsx(ee,{children:"Icon & text button"})]})}},l={args:{gap:ae.space.xlarge,children:e.jsxs(e.Fragment,{children:[e.jsx(g,{name:"activity",style:{pointerEvents:"none"}}),e.jsx(ee,{children:"Icon & text button"})]})}};var y,x,v;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:"{}",...(v=(x=r.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};var I,S,b;a.parameters={...a.parameters,docs:{...(I=a.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary'
+  }
+}`,...(b=(S=a.parameters)==null?void 0:S.docs)==null?void 0:b.source}}};var f,h,T;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(T=(h=s.parameters)==null?void 0:h.docs)==null?void 0:T.source}}};var j,D,B;n.parameters={...n.parameters,docs:{...(j=n.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    variant: 'tertiary'
+  }
+}`,...(B=(D=n.parameters)==null?void 0:D.docs)==null?void 0:B.source}}};var E,z,L;t.parameters={...t.parameters,docs:{...(E=t.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    variant: 'danger'
+  }
+}`,...(L=(z=t.parameters)==null?void 0:z.docs)==null?void 0:L.source}}};var V,k,F;o.parameters={...o.parameters,docs:{...(V=o.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(F=(k=o.parameters)==null?void 0:k.docs)==null?void 0:F.source}}};var G,M,P;c.parameters={...c.parameters,docs:{...(G=c.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...(P=(M=c.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var _,C,O;i.parameters={...i.parameters,docs:{...(_=i.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(O=(C=i.parameters)==null?void 0:C.docs)==null?void 0:O.source}}};var R,$,q;m.parameters={...m.parameters,docs:{...(R=m.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(q=($=m.parameters)==null?void 0:$.docs)==null?void 0:q.source}}};var w,A,H;u.parameters={...u.parameters,docs:{...(w=u.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    isLoading: true
+  }
+}`,...(H=(A=u.parameters)==null?void 0:A.docs)==null?void 0:H.source}}};var J,K,N;d.parameters={...d.parameters,docs:{...(J=d.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  name: 'Icon button',
+  args: {
+    children: <Icon name='activity' style={{
+      pointerEvents: 'none'
+    }} />
+  }
+}`,...(N=(K=d.parameters)==null?void 0:K.docs)==null?void 0:N.source}}};var Q,U,W;p.parameters={...p.parameters,docs:{...(Q=p.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  name: 'Icon & text button',
+  args: {
+    children: <>
+        <Icon name='activity' style={{
+        pointerEvents: 'none'
+      }} />
+        <Text>Icon & text button</Text>
+      </>
+  }
+}`,...(W=(U=p.parameters)==null?void 0:U.docs)==null?void 0:W.source}}};var X,Y,Z;l.parameters={...l.parameters,docs:{...(X=l.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  args: {
+    gap: tokens.space.xlarge,
+    children: <>
+        <Icon name='activity' style={{
+        pointerEvents: 'none'
+      }} />
+        <Text>Icon & text button</Text>
+      </>
+  }
+}`,...(Z=(Y=l.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};const de=["Default","Primary","Secondary","Tertiary","Danger","Small","Medium","Large","Disabled","Loading","IconButton","IconTextButton","Gap"];export{t as Danger,r as Default,m as Disabled,l as Gap,d as IconButton,p as IconTextButton,i as Large,u as Loading,c as Medium,a as Primary,s as Secondary,o as Small,n as Tertiary,de as __namedExportsOrder,ue as default};
---
+++ docs/assets/TextArea.stories-BfZbN05k.js
@@ -0,0 +1,64 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{r as z}from"./index-CBqU2yxZ.js";import{H as t,h as n,i as o,k as E,l as x}from"./Text--WihgvuV.js";import{L as H}from"./Logo-CuQCKLff.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";const{useArgs:k}=__STORYBOOK_MODULE_PREVIEW_API__,Y={title:"Components/Text area",component:t,args:{placeholder:"Placeholder text"},argTypes:{size:{defaultValue:{summary:"medium"}},isDisabled:{defaultValue:{summary:!1}},isReadOnly:{defaultValue:{summary:!1}}},parameters:{layout:"centered"},tags:["autodocs"]},s={render:r=>e.jsxs(n,{children:[e.jsx(o,{htmlFor:"textarea-example",children:"Textarea"}),e.jsx(t,{id:"textarea-example",...r})]})},l={args:{value:"Controlled text"},render:function(a){const[,V]=k(),_=c=>{var p;(p=a.onChange)==null||p.call(a,c),V({value:c})};return e.jsxs(n,{children:[e.jsx(o,{htmlFor:"textarea-controlled",children:"TextArea"}),z.createElement(t,{...a,key:a.value,id:"textarea-controlled",onChange:_,value:a.value,defaultValue:a.value})]})}},d={args:{size:"small"},render:r=>e.jsxs(n,{size:"small",children:[e.jsx(o,{htmlFor:"small-textarea-example",children:"Small textarea"}),e.jsx(t,{...r,id:"small-textarea-example",size:"small"})]})},u={args:{placeholder:void 0},render:r=>e.jsxs(n,{children:[e.jsx(o,{htmlFor:"do-you-use-vcs",children:"I have a decoration"}),e.jsxs(E,{children:[e.jsx(x,{borderRight:!0,children:"In the form of a text:"}),e.jsx(t,{...r,id:"do-you-use-vcs",rows:5}),e.jsx(x,{children:e.jsx(H,{})})]})]})},m={args:{isDisabled:!0,defaultValue:"Content"},render:r=>e.jsxs(n,{isDisabled:!0,children:[e.jsx(o,{htmlFor:"textarea-disabled",children:"I'm disabled"}),e.jsx(t,{id:"textarea-disabled",...r})]})},i={name:"Read only",args:{isReadOnly:!0,defaultValue:"Content"},render:r=>e.jsxs(n,{children:[e.jsx(o,{htmlFor:"textarea-readonly",children:"I'm read-only"}),e.jsx(t,{id:"textarea-readonly",...r})]})};var h,g,I;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => <FormGroup>
+      <InputLabel htmlFor='textarea-example'>Textarea</InputLabel>
+      <TextArea id='textarea-example' {...args} />
+    </FormGroup>
+}`,...(I=(g=s.parameters)==null?void 0:g.docs)==null?void 0:I.source}}};var b,f,F;l.parameters={...l.parameters,docs:{...(b=l.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    value: 'Controlled text'
+  },
+  render: function Component(args) {
+    const [, setArgs] = useArgs();
+    const onChangeHandler = (value: string) => {
+      args.onChange?.(value);
+      setArgs({
+        value
+      });
+    };
+    return <FormGroup>
+        <InputLabel htmlFor='textarea-controlled'>TextArea</InputLabel>
+        <TextArea {...args} key={args.value} id='textarea-controlled' onChange={onChangeHandler} value={args.value} defaultValue={args.value} />
+      </FormGroup>;
+  }
+}`,...(F=(f=l.parameters)==null?void 0:f.docs)==null?void 0:F.source}}};var y,j,v;d.parameters={...d.parameters,docs:{...(y=d.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  },
+  render: args => <FormGroup size='small'>
+      <InputLabel htmlFor='small-textarea-example'>Small textarea</InputLabel>
+      <TextArea {...args} id='small-textarea-example' size='small' />
+    </FormGroup>
+}`,...(v=(j=d.parameters)==null?void 0:j.docs)==null?void 0:v.source}}};var C,D,L;u.parameters={...u.parameters,docs:{...(C=u.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    placeholder: undefined
+  },
+  render: args => <FormGroup>
+      <InputLabel htmlFor='do-you-use-vcs'>I have a decoration</InputLabel>
+      <InputGroup>
+        <InputDecoration borderRight>In the form of a text:</InputDecoration>
+        <TextArea {...args} id='do-you-use-vcs' rows={5} />
+        <InputDecoration>
+          <Logo />
+        </InputDecoration>
+      </InputGroup>
+    </FormGroup>
+}`,...(L=(D=u.parameters)==null?void 0:D.docs)==null?void 0:L.source}}};var G,A,T;m.parameters={...m.parameters,docs:{...(G=m.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true,
+    defaultValue: 'Content'
+  },
+  render: args => <FormGroup isDisabled>
+      <InputLabel htmlFor='textarea-disabled'>I'm disabled</InputLabel>
+      <TextArea id='textarea-disabled' {...args} />
+    </FormGroup>
+}`,...(T=(A=m.parameters)==null?void 0:A.docs)==null?void 0:T.source}}};var R,S,O;i.parameters={...i.parameters,docs:{...(R=i.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  name: 'Read only',
+  args: {
+    isReadOnly: true,
+    defaultValue: 'Content'
+  },
+  render: args => <FormGroup>
+      <InputLabel htmlFor='textarea-readonly'>I'm read-only</InputLabel>
+      <TextArea id='textarea-readonly' {...args} />
+    </FormGroup>
+}`,...(O=(S=i.parameters)==null?void 0:S.docs)==null?void 0:O.source}}};const q=["Default","Controlled","Small","Decoration","Disabled","ReadOnly"];export{l as Controlled,u as Decoration,s as Default,m as Disabled,i as ReadOnly,d as Small,q as __namedExportsOrder,Y as default};
