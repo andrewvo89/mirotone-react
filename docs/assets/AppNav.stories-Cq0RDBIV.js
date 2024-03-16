0b3eda9d9a5b7a0896c29559e4fb21e4961ab110
andrewvo89 <andrewvo89@users.noreply.github.com>
2024-03-16 10:26:00 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@72690dedfc6223915eadc422bf70de4ae77cb56f 🚀

---
+++ docs/assets/AppNav.stories-Cq0RDBIV.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{F as i,t as r,B as d,I as c,T as l}from"./Text-BgNKOHsm.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";const h={title:"Recipes/App nav",render:s=>e.jsx(i,{alignItems:"center",gap:r.space.xsmall,style:{border:`1px solid ${r.color.indigo[300]}`,borderBottom:"none",borderRadius:`${r.borderRadius.large} ${r.borderRadius.large} 0 0`,padding:r.space.small,width:340},children:s.children}),parameters:{layout:"centered"},tags:["autodocs"]},t={name:"Back button",args:{children:e.jsxs(e.Fragment,{children:[e.jsx(d,{variant:"tertiary",children:e.jsx(c,{name:"arrow-left",style:{filter:"unset",pointerEvents:"none"}})}),e.jsx(l,{children:"Choose Cards"})]})}};var n,a,o;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: 'Back button',
+  args: {
+    children: <>
+        <Button variant='tertiary'>
+          <Icon name='arrow-left' style={{
+          filter: 'unset',
+          pointerEvents: 'none'
+        }} />
+        </Button>
+        <Text>Choose Cards</Text>
+      </>
+  }
+}`,...(o=(a=t.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const b=["BackButton"];export{t as BackButton,b as __namedExportsOrder,h as default};
---
+++ docs/assets/Heading.stories-CJel4RlU.js
@@ -0,0 +1,26 @@
+import{Q as y}from"./Text-BgNKOHsm.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";const Q={title:"Typography/Heading",component:y,argTypes:{level:{defaultValue:{summary:1}}},parameters:{layout:"centered"},tags:["autodocs"]},e={args:{children:"Heading 1",level:1}},r={args:{children:"Heading 2",level:2}},a={args:{children:"Heading 3",level:3}},n={args:{children:"Heading 4",level:4}},o={args:{children:"Colored heading",fontColor:"var(--red900)"}};var s,d,c;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 1',
+    level: 1
+  }
+}`,...(c=(d=e.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var t,i,l;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 2',
+    level: 2
+  }
+}`,...(l=(i=r.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};var g,m,p;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 3',
+    level: 3
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var u,H,h;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading 4',
+    level: 4
+  }
+}`,...(h=(H=n.parameters)==null?void 0:H.docs)==null?void 0:h.source}}};var v,C,f;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    children: 'Colored heading',
+    fontColor: 'var(--red900)'
+  }
+}`,...(f=(C=o.parameters)==null?void 0:C.docs)==null?void 0:f.source}}};const V=["Heading1","Heading2","Heading3","Heading4","Colored"];export{o as Colored,e as Heading1,r as Heading2,a as Heading3,n as Heading4,V as __namedExportsOrder,Q as default};
