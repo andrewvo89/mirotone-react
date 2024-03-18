b5a78177774fd15550434aab741ba859ef5856ff
andrewvo89 <andrewvo89@users.noreply.github.com>
2024-03-18 10:20:41 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@c16724d4319b3736bfb16d9259fbb936a4c19875 🚀

---
+++ docs/assets/Flex.stories-CUrPnuBZ.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{F as k,B as a,I as s,t as e}from"./Text-CR0FbxZk.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";import"./index-BtM5VmRH.js";const C={title:"Layout/Flex",component:k,args:{children:r.jsxs(r.Fragment,{children:[r.jsx(a,{children:r.jsx(s,{name:"arrow-left"})}),r.jsx(a,{children:r.jsx(s,{name:"arrow-up"})}),r.jsx(a,{children:r.jsx(s,{name:"arrow-down"})}),r.jsx(a,{children:r.jsx(s,{name:"arrow-right"})})]}),style:{border:`1px solid ${e.color.indigo[300]}`,borderRadius:e.borderRadius.large,padding:e.space.small}},parameters:{layout:"centered"},tags:["autodocs"]},o={},t={args:{flexDirection:"column"}},n={args:{flexDirection:"row"}},c={args:{flexWrap:"wrap"},render:p=>r.jsx(k,{...p,style:{...p.style,width:100},justifyContent:"space-evenly",rowGap:e.space.xsmall,columnGap:e.space.xsmall})},l={args:{gap:e.space.xsmall}};var m,i,d;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(d=(i=o.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var u,x,g;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    flexDirection: 'column'
+  }
+}`,...(g=(x=t.parameters)==null?void 0:x.docs)==null?void 0:g.source}}};var f,j,w;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    flexDirection: 'row'
+  }
+}`,...(w=(j=n.parameters)==null?void 0:j.docs)==null?void 0:w.source}}};var y,h,D;c.parameters={...c.parameters,docs:{...(y=c.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    flexWrap: 'wrap'
+  },
+  render: args => <Flex {...args} style={{
+    ...args.style,
+    width: 100
+  }} justifyContent='space-evenly' rowGap={tokens.space.xsmall} columnGap={tokens.space.xsmall} />
+}`,...(D=(h=c.parameters)==null?void 0:h.docs)==null?void 0:D.source}}};var G,F,S;l.parameters={...l.parameters,docs:{...(G=l.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    gap: tokens.space.xsmall
+  }
+}`,...(S=(F=l.parameters)==null?void 0:F.docs)==null?void 0:S.source}}};const E=["Default","Vertical","Horizontal","Wrap","Gap"];export{o as Default,l as Gap,n as Horizontal,t as Vertical,c as Wrap,E as __namedExportsOrder,C as default};
