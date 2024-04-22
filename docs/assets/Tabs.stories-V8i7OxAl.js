ad6ad5f461a0e9ffd37296ee7e63befe4bf3c7c1
andrewvo89 <andrewvo89@users.noreply.github.com>
2024-04-22 04:04:53 +0000

    Deploying to gh-pages from @ andrewvo89/mirotone-react@e22cca3d6611fa8749a283333e6e7f6a5988fe45 🚀

---
+++ docs/assets/Tabs.stories-V8i7OxAl.js
@@ -0,0 +1,8 @@
+import{y as t}from"./Text-Drqb0Ygr.js";import"./jsx-runtime-DRTy3Uxn.js";import"./index-BBkUAzwr.js";import"./index-BUy04y1F.js";import"./index-PqR-_bA4.js";const p={title:"Components/Tabs",component:t,args:{activeIndex:0,tabs:[{id:"1",badge:"1,000",children:"Tag"},{id:"2",badge:"2,201",children:"Tag"},{id:"3",badge:"2,858",children:"Tag"}]},parameters:{layout:"padded"},tags:["autodocs"]},e={argTypes:{activeIndex:{control:"inline-radio",options:[0,1,2]}}};var a,o,r;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  argTypes: {
+    activeIndex: {
+      control: 'inline-radio',
+      options: [0, 1, 2]
+    }
+  }
+}`,...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,p as default};
