3daf7cf791bdbb371cf134633b15a354effbbf3b
Andrew Vo-Nguyen <andrew.vo89@gmail.com>
2024-07-25 19:21:11 +1000

    Remove CenteredContainer component and update Centered.stories.tsx

--- src/layout/Centered/Centered.stories.tsx
+++ src/layout/Centered/Centered.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Centered, Heading, Paragraph } from '../..';
-import Container from '../../storybook/CenteredContainer';
 import Skeleton from '../../storybook/Skeleton';
 
 const meta: Meta<typeof Centered> = {
@@ -27,8 +26,19 @@ export const Default: Story = {
     ),
   },
   render: (args) => (
-    <Container>
+    <div
+      style={{
+        height: '500px',
+        padding: 'var(--space-small)',
+        width: '338px',
+        display: 'block',
+        border: '2px solid var(--indigo300)',
+        borderRadius: 'var(--border-radius-medium)',
+        margin: 'var(--space-small) auto',
+        background: 'var(--white)',
+      }}
+    >
       <Centered {...args} />
-    </Container>
+    </div>
   ),
 };
